3899f4534a8f9b6cc53df9ba6dfcc1bad5f9dbb1
Bob Dougherty <bobd@stanford.edu>
2018-07-20 21:24:41 -0700

    Compression for pandas objects

--- README.rst
+++ README.rst
@@ -13,6 +13,11 @@ Bug reports and merge requests are encouraged at the
 jsonpickle supports Python 2.7 and Python 3.4 or greater.
 
 
+Why jsonpickle?
+===============
+Data serialized with python's pickle (or cPicle or dill) is not easily readable outside of python. Using the json format, jsonpickle allows simple data types to be human-readable, and most fileds to be interpretable in any platform that supports json. E.g., unlike pickled ata, jsopickled data stored in an Amazon S3 bucket is indexible by Amazon's Athena.
+
+
 Install
 =======
 
--- jsonpickle/ext/pandas.py
+++ jsonpickle/ext/pandas.py
@@ -2,29 +2,73 @@ from __future__ import absolute_import
 
 import pandas as pd
 from io import StringIO
+import zlib
 
 from ..handlers import BaseHandler, register, unregister
+from ..util import b64decode, b64encode
+
 
 __all__ = ['register_handlers', 'unregister_handlers']
 
+class PandasProcessor():
+
+    def __init__(self, size_threshold=500, compression=zlib):
+        """
+        :param size_threshold: nonnegative int or None
+            valid values for 'size_threshold' are all nonnegative
+            integers and None
+            if size_threshold is None, dataframes are always stored as csv strings
+        :param compression: a compression module or None
+            valid values for 'compression' are {zlib, bz2, None}
+            if compresion is None, no compression is applied
+        """
+        self.size_threshold = size_threshold
+        self.compression = compression
+
+    def flatten_pandas(self, buf, data):
+        if self.size_threshold is not None and len(buf) > self.size_threshold:
+            if self.compression:
+                buf = self.compression.compress(buf.encode())
+                data['comp'] = True
+            data['values'] = b64encode(buf)
+            data['txt'] = False
+        else:
+            data['values'] = buf
+            data['txt'] = True
+        return data
+
+    def restore_pandas(self, data):
+        if data['txt']:
+            # It's just csv-encoded text...
+            csv = data['values']
+        else:
+            csv = b64decode(data['values'])
+            if data.get('comp', False):
+                csv = self.compression.decompress(csv).decode()
+        return csv
+
 
 class PandasDfHandler(BaseHandler):
+    pp = PandasProcessor()
 
     def flatten(self, obj, data):
-        data['DataFrame'] = obj.to_csv(index=False)
+        data = self.pp.flatten_pandas(obj.to_csv(index=False), data)
         return data
 
     def restore(self, data):
-        return pd.read_csv(StringIO(data['DataFrame']))
+        csv = self.pp.restore_pandas(data)
+        return pd.read_csv(StringIO(csv))
 
 class PandasSeriesHandler(BaseHandler):
+    pp = PandasProcessor()
 
     def flatten(self, obj, data):
-        data['Series'] = obj.to_csv()
+        data = self.pp.flatten_pandas(obj.to_csv(), data)
         return data
 
     def restore(self, data):
-        return pd.read_csv(StringIO(data['Series']), squeeze=True)
+        csv = self.pp.restore_pandas(data)
+        return pd.read_csv(StringIO(csv), squeeze=True)
 
 
 def register_handlers():
